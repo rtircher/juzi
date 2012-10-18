ba687b481a6aa61f402f7b15f3b03da7fc6ed053
Renaud Tircher <renaud.tircher@gmail.com>
2012-10-18 00:19:17 -0700

    Updating project.clj to have access to clj-json and to use port 4000 by default

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject juzi2 "0.1.0-SNAPSHOT"
+(defproject juzi "0.1.0-SNAPSHOT"
   :description "Quote wall app!"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/rtircher/juzi"
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-json "0.5.0"]
                  [compojure "1.1.3"]]
   :plugins [[lein-ring "0.7.5"]]
-  :ring {:handler juzi2.handler/app}
+  :ring {:handler juzi.handler/app
+         :port 4000}
   :dev-dependencies [[ring-mock "0.1.2"]])
