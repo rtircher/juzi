cae1727acc12557e2c091afc02c2ff163193654b
Renaud Tircher <renaud.tircher@gmail.com>
2012-10-17 23:05:28 -0700

    Adding models

---
+++ src/juzi2/models/quote.clj
@@ -0,0 +1,7 @@
+(ns juzi.models.quote)
+
+(defn quotes [wall-id]
+  {:quotes ["a quote" "another quote"]})
+
+(defn create-quote [wall-id quote-text]
+  {:create []})
---
+++ src/juzi2/models/wall.clj
@@ -0,0 +1,7 @@
+(ns juzi.models.wall)
+
+(defn wall [id]
+  {:id "random id" :quotes []})
+
+(defn create-wall []
+  {:id "random id" :quotes []})
