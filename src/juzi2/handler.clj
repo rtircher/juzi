e7b60b99f5ebc4e61793e90f3ebeea9846859e2f
Renaud Tircher <renaud.tircher@gmail.com>
2012-10-17 23:04:56 -0700

    Adding request logger

--- src/juzi2/handler.clj
+++ src/juzi2/handler.clj
@@ -1,11 +1,13 @@
 (ns juzi2.handler
   (:use compojure.core)
-  (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+  (:require [compojure.handler       :as handler]
+            [compojure.route         :as route]
+            [juzi.middlewares.logger :as logger]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
 (def app
-  (handler/site app-routes))
+  (handler/site app-routes)
+  (logger/wrap-request-loging))
