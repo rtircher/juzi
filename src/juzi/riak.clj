1f0e6cbb92abde424e1c24c869841a42a97a1b7a
Renaud Tircher <renaud.tircher@gmail.com>
2012-10-22 01:02:09 -0700

    Adding a id incrementer when creating quote

--- src/juzi/riak.clj
+++ src/juzi/riak.clj
@@ -1,11 +1,13 @@
 (ns juzi.riak
   (:use juzi.config)
-  (:require [clojurewerkz.welle.core :as wc]
-            [clojurewerkz.welle.kv   :as kv])
+  (:require [clojurewerkz.welle.core    :as wc]
+            [clojurewerkz.welle.kv      :as kv]
+            [clojurewerkz.welle.mr      :as mr])
   ;(:import com.basho.riak.client.http.util.Constants)
   )
 
 (def ^:private db-connected (ref false))
+(def ^:private quotes-bucket "quotes")
 
 (defn- ensure-db-connected []
   (dosync
@@ -13,12 +15,31 @@
      (wc/connect! (:db-url config))
      (ref-set db-connected true))))
 
+(defn- max-id-for [bucket]
+  (ensure-db-connected)
+  (read-string
+   (first
+    (mr/map-reduce
+     {:inputs bucket,
+      :query [{:map {:language "javascript",
+                     :source   "function(value) { return [value.key]}"}},
+              {:reduce {:language "javascript",
+                        :source   "
+function(valueList) {
+  return [ valueList.reduce(
+    function(acc, value){
+      return (+acc > +value) ? acc : value;
+    }
+  )];
+}"}}]}))))
+
+(def ^:private max-quotes-id (atom (max-id-for quotes-bucket)))
+(defn- quote-id []
+  (str (swap! max-quotes-id inc)))
+
+
 (defn create-quote! [quote]
   (ensure-db-connected)
-  (kv/store "quotes" "key" quote :content-type "application/clojure"))
-
-;; Need counter for each bucket to save current last id saved
-;; This need to be threadsafe
-;; Safe this in a riak bucket
-
-;; Need to return stored object
+  (let [id (quote-id)]
+    (kv/store quotes-bucket id quote :content-type "application/json")
+    (assoc quote :id id)))
